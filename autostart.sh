a3aa065c485ed79052fbd4b0f63c8bfd97e5bfe5
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2021-06-04 12:46:14 +0000

    first commit

---
+++ autostart.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+################################################################
+# 
+# Autostart setting
+# 
+# usage: ./autostart.sh --on/--off
+#
+#
+# @author Dr. Takeyuki UEDA
+# @copyright Copyright© Atelier UEDA 2018 - All rights reserved.
+#
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+CMD=$(basename -z $SCRIPT_DIR | sed 's/\x0//g')
+
+usage_exit(){
+  echo "Usage: $0 [--on]/[--off]" 1>&2
+  echo "  [--on]:               Set autostart as ON. " 			1>&2
+  echo "  [--off]:              Set autostart as OFF. " 		1>&2
+  echo "  [--status]:           Show current status. " 		 	1>&2
+  exit 1
+}
+
+on(){
+	sed -i "s@^WorkingDirectory=.*@WorkingDirectory=${SCRIPT_DIR}@" ${CMD}.service
+	sed -i "s@^ExecStart=.*@ExecStart=${SCRIPT_DIR}/${CMD}@" ${CMD}.service
+	sudo ln -s ${SCRIPT_DIR}\/${CMD}.service /etc/systemd/system/${CMD}.service
+#	sudo cp ${SCRIPT_DIR}\/${CMD}.service /etc/systemd/system/${CMD}.service
+	sudo systemctl daemon-reload
+	sudo systemctl enable ${CMD}.service
+	sudo systemctl start ${CMD}.service
+}
+
+off(){
+	sudo systemctl stop ${CMD}.service
+	sudo systemctl disable ${CMD}.service
+}
+
+status(){
+	sudo systemctl status ${CMD}.service
+}
+
+while getopts ":-:" OPT
+do
+  case $OPT in
+    -)
+				case "${OPTARG}" in
+					on)
+								on
+								;;
+					off)
+								off
+								;;
+					status)
+								status
+								;;
+				esac
+				;;
+    \?) usage_exit
+        ;;
+  esac
+done
